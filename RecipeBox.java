846d43e3864f911bbcc6c395ddcd7697c60863a4
BypassTheFedR <joshuaRthornburg@gmail.com>
2023-01-14 12:58:07 -0800

    ;;;

--- RecipeBox.java
+++ RecipeBox.java
@@ -62,7 +62,16 @@ public class RecipeBox {
 		System.out.println("Enter the name of the recipe: ");
 		// Recipe tmpRecipe = scnr.nextLine();
 		listOfRecipes.add(scnr.nextLine());
-		Recipe tmpRecipe = new Recipe(scnr.nextLine());
+		Recipe tmpRecipe = new Recipe();
+	}
+
+	public void deleteRecipe() {
+		Scanner deleteScnr = new Scanner(System.in);
+		String deleteRecipe = deleteScnr.nextLine();
+		for (int j = 0; j < listOfRecipes.size(); ++j) {
+			if (listOfRecipes.get(j).equalsIgnoreCase(deleteRecipe))
+				listOfRecipes.remove(j);
+		}
 	}
 	/**
 	* A variation of following menu method should be used as the actual main 
